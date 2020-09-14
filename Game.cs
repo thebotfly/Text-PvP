4dd102007426afd590e362e14840295487d58749
Lodis Perkins <lodisperkins44@gmail.com>
2020-09-14 11:05:50 -0500

    Created main battle loop

--- Game.cs
+++ Game.cs
@@ -4,24 +4,194 @@ using System.Text;
 
 namespace HelloWorld
 {
+    //Create a turn based PvP game. It should have a battle loop where both players
+    //must fight until one is dead. The game should allow players to upgrade their stats
+    //using items. Both players and items should be defined as structs. 
+
+    struct Player
+    {
+        public int health;
+        public int damage;
+    }
+
+    struct Item
+    {
+        public int statBoost;
+    }
+
+
     class Game
     {
+        bool _gameOver = false;
+        Player _player1;
+        Player _player2;
+        Item longSword;
+        Item dagger;
+
         //Run the game
         public void Run()
         {
-            
+            Start();
+
+            while(_gameOver == false)
+            {
+                Update();
+            }
+
+            End();
+
+        }
+
+        public void InitializePlayers()
+        {
+            _player1.health = 100;
+            _player1.damage = 5;
+
+            _player2.health = 100;
+            _player2.damage = 5;
+        }
+
+        public void InitializeItems()
+        {
+            longSword.statBoost = 15;
+            dagger.statBoost = 10;
+        }
+
+        //Displays two options to the player. Outputs the choice of the two options
+        public void GetInput(out char input, string option1, string option2, string query)
+        {
+            //Print description to console
+            Console.WriteLine(query);
+            //print options to console
+            Console.WriteLine("1." + option1);
+            Console.WriteLine("2." + option2);
+            Console.Write("> ");
+
+            input = ' ';
+            //loop until valid input is received
+            while (input != '1' && input != '2')
+            {
+                input = Console.ReadKey().KeyChar;
+                if(input != '1' && input != '2')
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+        }
+
+        //Equip items to both players in the beginning of the game
+        public void EquipItems()
+        {
+            //Get input for player one
+            char input;
+            GetInput(out input, "Longsword", "Dagger", "Welcome! Player one please choose a weapon.");
+            //Equip item based on input value
+            if (input == '1')
+            {
+                _player1.damage += longSword.statBoost;
+            }
+            else if (input == '2')
+            {
+                _player1.damage += dagger.statBoost;
+            }
+            Console.WriteLine("Player 1");
+            PrintStats(_player1);
+
+            //Get input for player two
+            GetInput(out input, "Longsword", "Dagger", "Welcome! Player two please choose a weapon.");
+            //Equip item based on input value
+            if (input == '1')
+            {
+                _player2.damage += longSword.statBoost;
+            }
+            else if (input == '2')
+            {
+                _player2.damage += dagger.statBoost;
+            }
+            Console.WriteLine("Player 2");
+            PrintStats(_player2);
+        }
+
+        public void ClearScreen()
+        {
+            Console.WriteLine("Press any key to continue");
+            Console.Write("> ");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void StartBattle()
+        {
+            ClearScreen();
+            Console.WriteLine("Now GO!");
+
+            while(_player1.health > 0 && _player2.health > 0)
+            {
+                //print player stats to console
+                Console.WriteLine("Player1");
+                PrintStats(_player1);
+                Console.WriteLine("Player2");
+                PrintStats(_player2);
+                //Player 1 turn start
+                //Get player input
+                char input;
+                GetInput(out input, "Attack", "NO", "Your turn Player 1");
+
+                if(input == '1')
+                {
+                    _player2.health -= _player1.damage;
+                    Console.WriteLine("Player 2 took " + _player1.damage + " damage!");
+                }
+                else
+                {
+                    Console.WriteLine("NO!!!!!");
+                }
+
+                GetInput(out input, "Attack", "NO", "Your turn Player 2");
+
+                if (input == '1')
+                {
+                    _player1.health -= _player2.damage;
+                    Console.WriteLine("Player 1 took " + _player2.damage + " damage!");
+                }
+                else
+                {
+                    Console.WriteLine("NO!!!!!");
+                }
+                Console.Clear();
+            }
+            if(_player1.health > 0)
+            {
+                Console.WriteLine("Player 1 wins!!1!1!!11!11?");
+            }
+            else
+            {
+                Console.WriteLine("Player 2 wins??????????");
+            }
+            ClearScreen();
+            _gameOver = true;
+        }
+
+
+        //Prints players stats to the console
+        public void PrintStats(Player player)
+        {
+            Console.WriteLine("Health: " + player.health);
+            Console.WriteLine("Damage: " + player.damage);
         }
 
         //Performed once when the game begins
         public void Start()
         {
-            
+            InitializePlayers();
+            InitializeItems();
         }
 
         //Repeated until the game ends
         public void Update()
         {
-            
+            EquipItems();
+            StartBattle();
         }
 
         //Performed once when the game ends
