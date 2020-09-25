4f55285271ccc6ee6d748dc6aa06bb2777bf526c
thebotfly <69916612+thebotfly@users.noreply.github.com>
2020-09-25 11:35:45 -0500

    added new classes

---
+++ Wizard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Wizard : Character
+    {
+        private float _mana;
+
+        public Wizard(float healthVal, string nameVal, float damageVal, float manaVal) : base(healthVal, nameVal, damageVal)
+        {
+            _mana = manaVal;
+        }
+
+        public override void Attack(Character enemy)
+        {
+            if(_mana >= 4)
+            {
+                float totalDamage = _damage + _mana * .25f;
+                _mana -= _mana * .25f;
+                enemy.TakeDamage(totalDamage);
+                return;
+            }
+            base.Attack(enemy);
+        }
+    }
+}
